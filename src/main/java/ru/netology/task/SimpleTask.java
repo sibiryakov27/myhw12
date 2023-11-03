4c33e56cc675e82dcf9dd7a4c679016248862c78
Ivan Sibiryakov <ivan.sibiryakov27@gmail.com>
2023-11-03 11:03:54 +0300

    complete task

---
+++ src/main/java/ru/netology/task/SimpleTask.java
@@ -0,0 +1,22 @@
+package ru.netology.task;
+
+public class SimpleTask extends Task {
+    protected String title;
+
+    public SimpleTask(int id, String title) {
+        super(id);
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public boolean matches(String query) {
+        if (title.contains(query)) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/ru/netology/TodosTest.java
@@ -0,0 +1,65 @@
+package ru.netology;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.task.Epic;
+import ru.netology.task.Meeting;
+import ru.netology.task.SimpleTask;
+import ru.netology.task.Task;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodosTest {
+
+    private Todos todos = new Todos();
+    private SimpleTask simpleTask;
+    private Epic epic;
+    private Meeting meeting;
+
+    @BeforeEach
+    public void setUp() {
+        simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        epic = new Epic(55, subtasks);
+
+        meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+    }
+
+    @Test
+    public void shouldFindThreeTasksOfDifferentType() {
+        Task[] expected = { simpleTask, epic, meeting };
+        Task[] actual = todos.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindSimpleTask() {
+        Task[] expected = { simpleTask };
+        Task[] actual = todos.search("родителям");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindEpicTask() {
+        Task[] expected = { epic };
+        Task[] actual = todos.search("Яйца");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMeetingTask() {
+        Task[] expected = { meeting };
+        Task[] actual = todos.search("Выкатка 3й версии");
+        assertArrayEquals(expected, actual);
+    }
+}
